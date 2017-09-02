d44b1505316a2f321891104776e41385edc07581
holmanhub <holman.eric85@gmail.com>
2017-09-02 10:23:24 -0500

    changed to es6

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var allProducts = [];
+const allProducts = [];
 
-var product1 = {
+const product1 = {
   name: "Mop Attire",
   imagePath: "./images/mopAttire.jpg",
   imageAlt: "Product: Mop Attire",
@@ -9,7 +9,7 @@ var product1 = {
   soldOut: false
 };
 
-var product2 = {
+const product2 = {
   name: "Taco Suit",
   imagePath: "./images/tacoSuit.jpg",
   imageAlt: "Product: Taco Suit",
@@ -18,7 +18,7 @@ var product2 = {
   soldOut: false
 };
 
-var product3 = {
+const product3 = {
   name: "Neck Decoration",
   imagePath: "./images/neckDecoration.jpg",
   imageAlt: "Product: Neck Decoration",
@@ -27,7 +27,7 @@ var product3 = {
   soldOut: false
 };
 
-var product4 = {
+const product4 = {
   name: "Head Ornament",
   imagePath: "./images/headOrnament.jpg",
   imageAlt: "Product: Head Ornament",
@@ -36,7 +36,7 @@ var product4 = {
   soldOut: true
 };
 
-var product5 = {
+const product5 = {
   name: "Boob Hat",
   imagePath: "./images/boobHat.jpg",
   imageAlt: "Product: Boob Hat",
@@ -51,13 +51,20 @@ allProducts.push(product3);
 allProducts.push(product4);
 allProducts.push(product5);
 
+const addNewProduct = (name, imagePath, imageAlt, description, price, soldOut) => {
+  var newProduct = {name, imagePath, imageAlt, description, price, soldOut};
+  allProducts.push(newProduct);
+}
+
+addNewProduct("Butt Cream", "https://upload.wikimedia.org/wikipedia/en/e/e8/Boudreauxs_Butt_Paste.jpg", "Butt Cream picture", "An American brand of skin cream that started out as a remedy for diaper rash.", 4.99, true)
+
 console.log("All my weird baby products: ", allProducts);
 
-var productContainer = document.getElementById("product-container");
+const productContainer = document.getElementById("product-container"); //could use let too
 
 
-function buildDomString(product){
-  var domString = "";
+const buildDomString = (product) => {
+  let domString = "";
 
   domString +=  '<section class="product">';
     domString +=    '<div class="title child">';
@@ -79,27 +86,25 @@ function buildDomString(product){
     return domString;
 }
 
-function printProductArrayToDom(productArray){
-  for (var i = 0; i < productArray.length; i ++) {
-    var currentProduct = productArray[i];
-    var productDomString = buildDomString(currentProduct)
+const printProductArrayToDom = (productArray) => {
+  for (let i = 0; i < productArray.length; i ++) {
+    const currentProduct = productArray[i];
+    const productDomString = buildDomString(currentProduct)
     productContainer.innerHTML += productDomString; 
   }
 }
 
-
-
 printProductArrayToDom(allProducts);
 
-var selectedCard;
+let selectedCard;
 
-document.getElementById('product-container').addEventListener('click', function(event){
+document.getElementById('product-container').addEventListener('click', (event) => {
   //console.log(event.target);
   changeBorder(event);
   printSelectedDescription();
 })
 
-function changeBorder(event){
+const changeBorder = (event) => {
   if (event.target.classList.contains('child')){
     selectedCard = event.target.parentNode;
   } else if (event.target.parentNode.parentNode.classList.contains('product')){
@@ -111,9 +116,9 @@ function changeBorder(event){
 selectedCard.classList.add('border-funsies');
 }
 
-function printSelectedDescription() {
+const printSelectedDescription = () => {
   //console.log(selectedCard);
-  var description = selectedCard.childNodes[2].childNodes[0].innerHTML;
+  const description = selectedCard.childNodes[2].childNodes[0].innerHTML;
   console.log(description);
 }
 
